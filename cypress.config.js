919b2a26473c12a00fbe55ecea553ab695f89161
DuyTran95x <duy.tran@1bit.com.ph>
2024-03-23 01:14:57 +0700

    new-updates

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'docs',
     charts: true,
     reportPageTitle: "Casino",
     // embeddedScreenshots: true,
